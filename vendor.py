4fa288b31233dff6f8219d8b4581899e97f9969c
ekumamait <ericebuluochol@gmail.com>
2018-09-03 00:18:52 +0300

    modify vendor.py to concatenate string and int for balance [#160217247]

--- vendor.py
+++ vendor.py
@@ -16,78 +16,80 @@ def cola():
     price = int(input("Enter 100 to buy a coca-cola: "))
     cost = 100
     if price == cost:
-        return "Coca-Cola"
+        print("Here is your Coca-Cola. Enjoy")
     elif price < cost:
         newprice = int(input("Enter more to buy a coca-cola: "))
         if newprice + price == cost:
-            return "Coca-Cola"
+            print("Here is your Coca-Cola. Enjoy")
         else:
-            return "oops"     
+            print("oops! invalid input")     
     elif price > cost:
         balance = price - cost
-        return "Coca-Cola " + balance        
+        print("Here is your Coca-Cola. Enjoy & Balance of "  + str(balance))        
     else:
-        return "oops"
+        print("oops! invalid input")
 
 def pepsi():
     """A function to operate on pepsi option"""
     price = int(input("Enter 50 to buy a pepsi: "))
     cost = 50
     if price == cost:
-        return "Pepsi"
+        print("Here is your Pepsi. Enjoy")
     elif price < cost:
         newprice = int(input("Enter more to buy a pepsi: "))
         if newprice + price == cost:
-            return "Pepsi"
+            ("Here is your Pepsi. Enjoy")
         else:
-            return "oops"    
+            print("oops! invalid input")    
     elif price > cost:
         balance = price - cost
-        return "Coca-Cola " + balance        
+        print("Here is your Pepsi. Enjoy & Balance of " + str(balance))        
     else:
-        return "oops"    
+        print("oops! invalid input")    
 
 def sprite():
     """A function to operate on sprite option"""
     price = int(input("Enter 70 to buy a sprite: "))
     cost = 70
     if price == cost:
-        return "Sprite"
+        print("Here is your Sprite. Enjoy")
     elif price < cost:
         newprice = int(input("Enter more to buy a sprite: "))
         if newprice + price == cost:
-            return "Sprite"
+            print("Here is your Sprite. Enjoy")
         else:
-            return "oops"    
+            print("oops! invalid input")    
     elif price > cost:
         balance = price - cost
-        return "Sprite " + balance
+        print("Here is your Sprite. Enjoy & Balance of " + str(balance))
     else:
-        return "oops" 
+        print("oops! invalid input") 
 
 def dew():  
     """A function to operate on Mountain option"""
     price = int(input("Enter 80 to buy a Mountain option: "))
     cost = 80
     if price == cost:
-        return "Mountain Dew"  
+        print("Here is your Mountain Dew. Enjoy")  
     elif price < cost:
         newprice = int(input("Enter more to buy a Mountain option: "))
         if newprice + price == cost:
-            return "Mountain Dew"
+            print("Here is your Mountain Dew. Enjoy")
         else:
-            return "oops"    
+            print("oops! invalid input")    
     elif price > cost:
         balance = price - cost
-        return "Mountain Dew " + balance
+        print("Here is your Mountain Dew. Enjoy & Balance of" + str(balance))
     else:
-        return "oops"
+        print("oops! invalid input")
 
-switcher = {"one": cola, "two": pepsi, "three": sprite, "four": dew}
-lines = ["one", "two", "three", "four"]
+switcher = {1: cola, 2: pepsi, 3: sprite, 4: dew}
+lines = [1, 2, 3, 4]
 
 for line in lines:
     func = switcher[line]
     func()
-            
+
+# func = switcher.get(choice, "invalid input")
+# return fun()
  
